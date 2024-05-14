0f98d7b260fadb4d92a7d92296cbd297fd4fa331
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-14 14:35:24 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,58 @@
+// let qoshish = +prompt("birinchi soni kiriting")
+// let qoshish1= +prompt("ikkinchi soni kiriting")
+// let a;
+// let b;
+// function  qoshish5 (a,b) {
+//     let c;
+//     c = a + b
+//     console.log(c);
+// }
+// qoshish5(qoshish,qoshish1);
+
+// function number() {
+//     let a =  Number ("12");
+// console.log(a);
+
+// }
+// number()
+
+// let array = [2,4, 9, 10, 45, 67, 8, 90]
+// for (let i = 0;  i< array.length; i++) {
+//     let check = array[i] % 2;
+//     if (check === 0) {
+//         console.log("Bu juft son" +  array[i]);
+        
+//     }else{
+//         console.log("Bu toq son" + array[i]);
+//     }
+// }
+
+// let mars = prompt()
+// if (mars === "MARS") {
+//     console.log("MARS");
+// }else{
+//     console.log("MARS IT SCHOOL");
+// }
+
+// let array = ["tort","notubuk","uchburchak", "uch", "besh"];
+// for (let i = 0; i < array.length; i++)
+//  {
+//     if (array < 5 ) {
+//         console.log("tort,uch,besh");
+//     }else{
+//         console.log("notubuk,uchburchak");
+//     }
+// }
+
+// let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,23,24,25,26,27,28,29,3031,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+// for (let i = 0;  i< array.length; i++) {
+//     let check = array[i] % 2;
+//     if (check === 0) {
+//         console.log("Bu juft son" +  array[i]);
+        
+//     }else{
+//         console.log("Bu toq son" + array[i]);
+//     }
+// }
+
+
